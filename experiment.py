05c5ebe399507ec43b02aac60e2372430e3cfa14
Matteo Papini <matteo.papini@mail.polimi.i>
2017-04-18 11:38:46 +0200

    gpomdp 4 results

--- experiment.py
+++ experiment.py
@@ -3,9 +3,9 @@ import subprocess
 for delta in [float(x)/100 for x in range(100,70,-5)]:
     max_N = 30000000 
 
-    filename = "results/adabatch_gpomdp_d{}_max_{}_e3".format(delta,max_N)
+    filename = "results/adabatch_gpomdp_d{}_max_{}_e4".format(delta,max_N)
     filename = filename.replace(".","_")
     filename = filename + ".out"
 
-    subprocess.call(" python lqg/lqg_adabatch.py 1000 {} {} {}".format(delta,filename,max_N), shell=True)
+    subprocess.call(" python lqg/lqg_adabatch.py 100000 {} {} {}".format(delta,filename,max_N), shell=True)
 
