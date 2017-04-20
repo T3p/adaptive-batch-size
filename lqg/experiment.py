0e415e21d1ae5a2f577ca8da97d36fd574cd7326
Matteo Papini <matteo.papini@mail.polimi.i>
2017-04-20 21:37:07 +0200

    hoeffding experiments

--- lqg/experiment.py
+++ lqg/experiment.py
@@ -1,12 +1,12 @@
 import subprocess
 import os
 
-for delta in [float(x)/100 for x in range(100,70,-5)]:
+for delta in [float(x)/100 for x in range(100,0,-5)]:
     max_N = 30000000 
 
-    filename = "results/adabatch_gpomdp_d{}_max_{}_unbiased_1".format(delta,max_N)
+    filename = "results/adabatch_gpomdp_d{}_hoeffding".format(delta)
     filename = filename.replace(".","_")
     filename = filename + ".out"
 
-    subprocess.call("python lqg_adabatch.py 1000 {} {} {}".format(delta,filename,max_N), shell=True)
+    subprocess.call("python lqg_hoeffding.py 1000 {} {}".format(delta,filename), shell=True)
 
