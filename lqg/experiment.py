4dd206eeac634e518ec23b5549941df92c84761b
Matteo Papini <matteo.papini@mail.polimi.i>
2017-04-25 21:22:58 +0200

    other results

--- lqg/experiment.py
+++ lqg/experiment.py
@@ -4,7 +4,7 @@ import os
 for delta in [float(x)/100 for x in range(100,0,-5)]:
     max_N = 30000000 
 
-    filename = "results/adabatch_gpomdp_d{}_hoeffding_const".format(delta)
+    filename = "results/adabatch_gpomdp_d{}_hoeffding_theo".format(delta)
     filename = filename.replace(".","_")
     filename = filename + ".out"
 
