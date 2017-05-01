2abeac53eb026e09662238b103c31a5b94c233e4
Matteo Papini <papini@kth.se>
2017-05-01 18:42:39 +0200

    changed experiment order

--- lqg/experiment.py
+++ lqg/experiment.py
@@ -5,14 +5,15 @@ import os
 max_N = 20000000
 N_min = 10
 N_max = 10000
-for estimator in range(2):
-    for bound in range(4):
-        if estimator+bound==1:
-            continue
-        for delta in [0.95,0.75,0.5]:
-            filename = "results/final/adabatch_est{}_bound{}__delta{}".format(estimator,bound,delta)
-            filename = filename.replace(".","_")
-            filename = filename + ".out"
+for i in range(5):
+    for delta in [0.95,0.75,0.5,0.25]:
+        for estimator in range(2):
+            for bound in range(4):
+                if estimator+bound==1:
+                    continue
+                filename = "results/final/adabatch_est{}_bound{}__delta{}_sample{}".format(estimator,bound,delta,i+1)
+                filename = filename.replace(".","_")
+                filename = filename + ".out"
 
-            subprocess.call("python lqg_adabatch.py {} {} {} {} {} {} {}".format(N_min,N_max,delta,estimator,bound,filename,max_N), shell=True)
+                subprocess.call("python lqg_adabatch.py {} {} {} {} {} {} {}".format(N_min,N_max,delta,estimator,bound,filename,max_N), shell=True)
 
