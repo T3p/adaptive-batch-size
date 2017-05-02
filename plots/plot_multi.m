8614a84541aad4a0f8b85cc0171b06f4ee80346c
Matteo Papini <papini@kth.se>
2017-05-02 18:45:01 +0200

    small fix to bernstein

--- plots/plot_multi.m
+++ plots/plot_multi.m
@@ -2,38 +2,22 @@ close all
 clear
 clc
 
-M = importdata('~/adaptive-batch-size/lqg/results/adabatch_gpomdp_d1_0_max30.out',' ',1);
+M = importdata('~/adaptive-batch-size/lqg/results/final/adabatch_est1_bound1__delta0_95_sample1.out',' ',1);
 figure
 hold on
 color = 'b';
 plot_scaledData
 J_avg = sum(realJ.*batchsize)/sum(batchsize)
 
-M = importdata('~/adaptive-batch-size/lqg/results/adabatch_gpomdp_d0_9_max30.out',' ',1);
+M = importdata('~/adaptive-batch-size/lqg/results/final/adabatch_est1_bound2__delta0_95_sample1.out',' ',1);
 color = 'r';
 plot_scaledData
 J_avg = sum(realJ.*batchsize)/sum(batchsize)
 
 
-M = importdata('~/adaptive-batch-size/lqg/results/adabatch_gpomdp_d0_8_max30.out',' ',1);
+M = importdata('~/adaptive-batch-size/lqg/results/final/adabatch_est1_bound3__delta0_95_sample1.out',' ',1);
 color = 'g';
 plot_scaledData
 J_avg = sum(realJ.*batchsize)/sum(batchsize)
 
-M = importdata('~/adaptive-batch-size/lqg/results/adabatch_gpomdp_d0_75_max30.out',' ',1);
-color = 'y';
-plot_scaledData
-J_avg = sum(realJ.*batchsize)/sum(batchsize)
-
-M = importdata('~/adaptive-batch-size/lqg/results/adabatch_gpomdp_d0_95_hoeffding_theo.out',' ',1);
-color = 'b--';
-plot_scaledData
-J_avg = sum(realJ.*batchsize)/sum(batchsize)
-
-M = importdata('~/adaptive-batch-size/lqg/results/adabatch_bernstein2_2_1000_99.out',' ',1);
-M.data(:,3) = [];
-color = 'r--';
-plot_scaledData
-J_avg = sum(realJ.*batchsize)/sum(batchsize)
-
-legend('delta = 1.0','delta = 0.9','delta = 0.8','delta = 0.75', 'hoeff delta = 0.95','bernstein delta = 0.99')
+legend('chebyshev', 'hoeffding', 'bernstein')
