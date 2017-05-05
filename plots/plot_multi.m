dc83c59095b4acc379eb96415c637e626938fcdf
Matteo Papini <papini@kth.se>
2017-05-05 12:24:39 +0200

    computed confidence intervals

--- plots/plot_multi.m
+++ plots/plot_multi.m
@@ -9,14 +9,14 @@ color = 'b';
 plot_scaledData
 J_avg = sum(realJ.*batchsize)/sum(batchsize)
 
-M = importdata('~/adaptive-batch-size/lqg/results/final/adabatch_est1_bound1__delta0_75_sample1.out',' ',1);
+M = importdata('~/adaptive-batch-size/lqg/results/final/adabatch_est1_bound1__delta0_95_sample2.out',' ',1);
 color = 'r';
 plot_scaledData
 J_avg = sum(realJ.*batchsize)/sum(batchsize)
 
-M = importdata('~/adaptive-batch-size/lqg/results/final/adabatch_est1_bound1__delta0_5_sample1.out',' ',1);
+M = importdata('~/adaptive-batch-size/lqg/results/final/adabatch_est1_bound1__delta0_95_sample3.out',' ',1);
 color = 'g';
 plot_scaledData
 J_avg = sum(realJ.*batchsize)/sum(batchsize)
 
-legend('.95', '.75', '.5')
+legend('.95', '.95', '.95')
